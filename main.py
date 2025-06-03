af1e0a8af7230e6807b3c42d01f216b81312c4ed
Aakif9866 <aakif9866@gmail.com>
2025-06-03 12:51:30 +0530

    prepare base model done

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = 'DATA_INGESTION_STAGE'
 
 
 try:
+    logger.info(f"**********************")
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
     obj.main()
@@ -13,3 +15,14 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+try:
+        logger.info(f"**********************")
+        logger.info(f">>>>>>  stage {STAGE_NAME} stared <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>  stage {STAGE_NAME} completed <<<<<<\n\nx=======x")
+except Exception as e:
+    
+    logger.exception(e)
+    raise e
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,7 @@
 from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml,create_directories
-from cnnClassifier.entity.config_entity import DataIngestionConfig
-from cnnClassifier.components.data_ingestion import DataIngestion
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig)
+
 
 
 class ConfigurationManager:
@@ -42,3 +42,23 @@ class ConfigurationManager:
         return data_ingestion_config
       
 
+    # this will goto configuration.py
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        # these are the contents we are reading from config.yaml file
+        
+        
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+
+        return prepare_base_model_config
